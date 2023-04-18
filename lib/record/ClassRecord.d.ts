cef9df0319d1ffe7f4d050bd707b54542caf41c2
Mac-A-4 <malcolm.alvarez9898@gmail.com>
2023-04-18 15:55:04 -0700

    `Build`

---
+++ lib/record/ClassRecord.d.ts
@@ -0,0 +1,9 @@
+export interface ClassRecord {
+    classId: string;
+    transcriptId: string;
+    subject: string;
+    name: string;
+    grade: number;
+    year: string;
+    provider: string;
+}
---
+++ src/record/ClassRecord.ts
@@ -0,0 +1,18 @@
+
+export interface ClassRecord {
+
+  classId: string;
+
+  transcriptId: string;
+
+  subject: string;
+
+  name: string;
+
+  grade: number;
+
+  year: string;
+
+  provider: string;
+
+}
