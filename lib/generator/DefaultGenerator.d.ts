fc39003733e12a5720b906a96bfa9dba9b8c6c26
Mac-A-4 <malcolm.alvarez9898@gmail.com>
2023-04-21 20:50:00 -0700

    `Build`

--- lib/generator/DefaultGenerator.d.ts
+++ lib/generator/DefaultGenerator.d.ts
@@ -4,4 +4,12 @@ import { BaseGenerator } from './BaseGenerator';
 export declare class DefaultGenerator extends BaseGenerator {
     constructor(account: AccountRecord, transcript: TranscriptRecord);
     onGenerate(): Promise<void>;
+    private generateRoot;
+    private generateHeader;
+    private generateInformation;
+    private generateStudentInformation;
+    private generateStudentName;
+    private generateStudentGraduationDate;
+    private generateSchoolInformation;
+    private generateInformationEntry;
 }
